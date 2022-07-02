003455a374338774ca6514555333d8812ff59a0c
calos <fxiaoyu97@qq.com>
2022-07-02 23:30:21 +0800

    vue+ts+vite二次封装element-plus

--- docs/.vuepress/autoSidebar.ts
+++ docs/.vuepress/autoSidebar.ts
@@ -3,11 +3,11 @@ import path from 'path'
 const docsRoot = path.join(__dirname, '..')
 
 class SidebarItem {
-  text: string;
-  collapsible?: boolean;
-  children: Array<any>;
+  text: string
+  collapsible?: boolean
+  children: Array<any>
   constructor(text: string) {
-    this.text = text;
+    this.text = text
   }
 }
 
@@ -32,27 +32,31 @@ function getPathText(file: string): string {
  */
 function needFilter(file: string): boolean {
   // vuepress配置文件
-  if (file === '.vuepress') return true;
+  if (file === '.vuepress') return true
   // 图片文件夹
-  if (file === 'img'||file === 'image') return true;
+  if (file === 'img' || file === 'image') return true
   // README文件
-  if(file === 'README.md' || file === 'readme.md') return true;
+  if (file === 'README.md' || file === 'readme.md') return true
   // 图片文件
   // let arr = file.split('.');
   // if(arr.lastIndexOf('jpg')!=-1 || arr.lastIndexOf('jpeg')!=-1 || arr.lastIndexOf('png')!=-1) return true;
-  return false;
+  return false
 }
- /**
-  * 获取指定文件夹的文章目录
-  * 
-  * @param filePath 文章文件夹路径
-  * @param fileDepth 小于等于这个深度的文件夹的文章都不折叠
-  * @param moreArr 侧边栏还需要额外添加的目录
-  * @returns 侧边栏目录
-  */
-export function getChildren(filePath: string, fileDepth: number = 0, moreArr: Array<any> = new Array<any>()): Array<any> {
-  let result = Array<any>();
-  let rootPath = getRootPath(filePath);
+/**
+ * 获取指定文件夹的文章目录
+ *
+ * @param filePath 文章文件夹路径
+ * @param fileDepth 小于等于这个深度的文件夹的文章都不折叠
+ * @param moreArr 侧边栏还需要额外添加的目录
+ * @returns 侧边栏目录
+ */
+export function getChildren(
+  filePath: string,
+  fileDepth: number = 0,
+  moreArr: Array<any> = new Array<any>()
+): Array<any> {
+  let result = Array<any>()
+  let rootPath = getRootPath(filePath)
   let files = fs.readdirSync(rootPath)
   // 排序
   sortArr(files)
@@ -61,14 +65,14 @@ export function getChildren(filePath: string, fileDepth: number = 0, moreArr: Ar
       let docsPath = `${filePath}/${file}`
       const stat = fs.statSync(path.join(rootPath, file))
       if (stat.isDirectory()) {
-        let item = new SidebarItem(getPathText(file));
-        item.collapsible = fileDepth <= 0 ? false : true;
-        item.children = getChildren(docsPath, fileDepth + 1);
+        let item = new SidebarItem(getPathText(file))
+        item.collapsible = fileDepth <= 0 ? false : true
+        item.children = getChildren(docsPath, fileDepth + 1)
         result.push(item)
       } else {
         result.push(`/${docsPath}`)
       }
     }
   })
-  return result.concat(moreArr);
+  return result.concat(moreArr)
 }
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,81 +2,79 @@ import { defaultTheme, defineUserConfig } from 'vuepress'
 import { getChildren } from './autoSidebar'
 
 export default defineUserConfig({
-    lang: 'zh-CN',
-    title: '三更编程',
-    description: '三更编程，洛小豆',
-    head: [
-        // 增加一个自定义的 favicon(网页标签的图标)
-        // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
-        // 即 docs/.vuepress/public/img/geass-bg.ico
-        ['link', {
-            rel: 'icon',
-            href: '/img/geass-bg.ico'
-        }],
+  lang: 'zh-CN',
+  title: '三更编程',
+  description: '三更编程，洛小豆',
+  head: [
+    // 增加一个自定义的 favicon(网页标签的图标)
+    // 这里的 '/' 指向 docs/.vuepress/public 文件目录
+    // 即 docs/.vuepress/public/img/geass-bg.ico
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/img/geass-bg.ico'
+      }
+    ]
+  ],
+  base: '/', // 这是部署到github相关的配置
+  theme: defaultTheme({
+    logo: 'https://vuejs.org/images/logo.png',
+    sidebarDepth: 1, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: false,
+    contributors: false,
+    navbar: [
+      // {
+      //     text: '前端',
+      //     children: ['/vue/']
+      // },
+      // {
+      //     text: '后端',
+      //     link: '/FQA/'
+      // },
+      {
+        text: 'Java',
+        children: [
+          {
+            text: 'Java基础',
+            link: '/Java基础/',
+            activeMatch: '^/Java基础/'
+          }
+        ]
+      },
+      {
+        text: '编程工具',
+        children: [
+          {
+            text: 'Git',
+            link: '/git/',
+            activeMatch: '^/git/'
+          }
+        ]
+      },
+      {
+        text: '面试宝典',
+        link: '/面试宝典/'
+      },
+      {
+        text: '算法',
+        link: '/算法/'
+      },
+      {
+        text: '项目笔记',
+        link: '/项目笔记/'
+      }
+      // {
+      //     text: '关于我',
+      //     link: '/FQA/'
+      // },
     ],
-    base: '/', // 这是部署到github相关的配置
-    theme: defaultTheme({
-        logo: 'https://vuejs.org/images/logo.png',
-        sidebarDepth: 1, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-        lastUpdated: false,
-        contributors: false,
-        navbar: [
-            // {
-            //     text: '前端',
-            //     children: ['/vue/']
-            // },
-            // {
-            //     text: '后端',
-            //     link: '/FQA/'
-            // },
-            {
-                text: 'Java',
-                children: [
-                    {
-                        text:'Java基础',
-                        link:'/Java基础/',
-                        activeMatch:'^/Java基础/'
-                    }
-                ]
-            },
-            {
-                text: '编程工具',
-                children: [
-                    {
-                        text:'Git',
-                        link:'/git/',
-                        activeMatch:'^/git/'
-                    }
-                ]
-            },
-            {
-                text: '面试宝典',
-                link: '/面试宝典/'
-            },
-            {
-                text: '算法',
-                link: '/算法/'
-            },
-            {
-                text: '项目笔记',
-                children: [
-                    {
-                        text:'Vue3+TS二次重构element-plus组件',
-                        link:'/Java基础/',
-                        activeMatch:'^/Java基础/'
-                    }
-                ]
-            },
-            // {
-            //     text: '关于我',
-            //     link: '/FQA/'
-            // },
-        ],
-        sidebar: {
-            '/Java%E5%9F%BA%E7%A1%80/':getChildren(`Java基础`),
-            '/FQA/': getChildren(`FQA`),
-            '/git/': getChildren(`git`),
-            '/%E9%9D%A2%E8%AF%95%E5%AE%9D%E5%85%B8/': getChildren(`面试宝典`),
-        }
-    }),
+    sidebar: {
+      '/Java%E5%9F%BA%E7%A1%80/': getChildren(`Java基础`),
+      '/FQA/': getChildren(`FQA`),
+      '/git/': getChildren(`git`),
+      '/%E9%9D%A2%E8%AF%95%E5%AE%9D%E5%85%B8/': getChildren(`面试宝典`),
+      '/%E9%A1%B9%E7%9B%AE%E7%AC%94%E8%AE%B0/': getChildren(`项目笔记`, 1)
+    }
+  })
 })
